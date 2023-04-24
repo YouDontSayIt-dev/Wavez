0a7300a468c3fb601b736190a10b635eabc72abe
blancojames88 <blancojames88@gmail.com>
2023-04-25 01:22:46 +0800

    Deleted things from each div in exp.html
    Read note everytime you use exp.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,12 @@ module.exports = {
         'custom-color': '#04151D',
         // custom-color for the whole website body
 
+        // custom-color for the sidebar buttons
         'custom-color-sidebar-brown': '#9A7F69',
+
+        // custom-color for when hovering the sidebar buttons
+        'custom-color-sidebar-brown-hover': '#9A7F69',
+        
       },
       fontFamily: {
         'alata': ['Alata', 'sans-serif'],
