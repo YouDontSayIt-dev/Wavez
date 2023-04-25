32b274d80d347eccbdbfbcaeca993e4388708a0e
ImperialDuck-dev <mjmuyco13@gmail.com>
2023-04-26 02:30:57 +0800

    added final header and footer

--- include/footer.js
+++ include/footer.js
@@ -5,11 +5,37 @@ class Footer extends HTMLElement {
 
     connectedCallback(){
         this.innerHTML = `
-    <footer>
-    <div class = "footer">
-    <p1 class = "Footer-Co_txt">Company</p1>
-    </div>
-    </footer>
+        <footer class="bg-custom-color font-alata">
+        <div class ="flex max-w-[270px] my-auto mx-auto items-center">
+            <div class = "grid grid-cols-4 gap-x-[150px]">
+                <div class="mb-5 text-white font-[19px] leading-[26px]">
+                    <h4 class="">Company</h4>
+                    <ul>
+                        <li class="text-gray-600">About</li>
+                        <li class="text-gray-600">Jobs</li>
+                        <li class="text-gray-600">For The Record</li>
+                    </ul>
+                </div>
+                <div class="mb-5 text-white font-[19px] leading-[26px]">
+                    <h4 class="">Communities</h4>
+                    <ul>
+                        <li class="text-gray-600">Developers</li>
+                        <li class="text-gray-600">Advertising</li>
+                        <li class="text-gray-600">Investors</li>
+                    </ul>
+                </div>
+                <div class="mb-5 text-white font-[19px] leading-[26px]">
+                    <h4 class="">Useful Links</h4>
+                    <ul>
+                        <li class="text-gray-600">Support</li>
+                        <li class="text-gray-600">Legal</li>
+                        <li class="text-gray-600">Privacy</li>
+                        <li class="text-gray-600">Cookies</li>
+                    </ul>
+                </div>
+            </div>
+            </div>
+        </footer>
         `;
     }        
 }
--- include/header.js
+++ include/header.js
@@ -5,14 +5,20 @@ class Header extends HTMLElement {
 
     connectedCallback(){
         this.innerHTML = `
-    <header>
-        <div class = header>
-            <h1 class = "Logo-name">WAVEZ</h1>
-                <p class = "Header-login">Login</p>
-                <p class = "Header-sign_up">Sign Up</p>
-                    <button class = "Header-btn">Get Started</button>
-                    <img class = "Logo" src = "assets/wavesLogo.png" alt = "Logo">
+        <header class = "bg-custom-color font-alata sticky">
+        <div class ="flex h-[147px] items-center justify-end">
+            <div class="w-[150px] mr-auto flex items-center">
+                <img class =" px-8 py-4 " src="/assets/wavesLogo.png">
+                <div class="text-white px-4 py-4 tracking-[0.18rem] text-[44px] leading-[53px] ">WAVES</div>
+            </div>
+            <div class="px-6 py-4 text-white font-[19px] leading-[26px]">Login</div>
+            <div class="px-6 py-4 text-white font-[19px] leading-[26px]">Sign Up</div>
+            <div class = "px-8 py-4 text-black font-[19px] leading-[26px]">
+                <button class="bg-custom-color-sidebar-brown rounded-full py-2 px-4 font-bold">GET STARTED</button>
+            </div>
+                     
         </div>
+
     </header>
         `;
     }
