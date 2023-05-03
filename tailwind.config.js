f11138263508d7c668c81c8f2196054ec0d68b5f
ImperialDuck-dev <mjmuyco13@gmail.com>
2023-05-03 20:30:08 +0800

    config fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.{html,js}","./include/*.{js}"],
+  content: ["./dist/*.{html,js}","./include/*.js"],
   theme: {
     extend: {
       colors: {
