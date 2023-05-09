c2e27d4ef117014493a6e8bbc91eb76c60815804
websiteyarn <104047557+websiteyarn@users.noreply.github.com>
2023-05-09 20:16:46 +0800

    sidebar profile bar OK
    playlist

--- include/playlist.js
+++ include/playlist.js
@@ -100,21 +100,21 @@ let generateTrackList = () => {
         let {name, artist, album, duration, image} = x;
         return `
             <div class="flex text-custom-color-song-more my-2 text-xs song hover:cursor-pointer">
-                <p class="w-1/12 text-base pl-10 my-auto">${count}</p>
+                <p class="w-1/12 text-base pl-10 my-auto large:pl-8 mid:pl-6 mid2:pl-6 xsmall:pl-3">${count}</p>
                 <!-- Img and Title/Artist -->
-                <div class="flex w-4/12"> 
+                <div class="flex w-4/12 xlarge:pl-4 mid:w-8/12 small:w-10/12"> 
                     <img src="/assets/playlist-mg-assets/${image}" alt="Song" class="w-12 rounded-md"> 
-                    <div class="flex flex-col justify-center">
-                        <p class="text-custom-color-song-font ml-4">${name}</p>
-                        <p class="ml-4">${artist}</p>
+                    <div class="flex flex-col justify-center ml-4">
+                        <p class="text-custom-color-song-font">${name}</p>
+                        <p>${artist}</p>
                     </div>
                 </div>
-                <p class="w-4/12 my-auto">${album}</p>
-                <span class="material-symbols-rounded w-1/12 m-auto">favorite</span>
-                <p class="w-1/12 my-auto">${duration}</p>
-                <div class="w-1/12 my-auto text-custom-color optcontainer">
-                    <i class="fa-solid fa-ellipsis fa-xl option"></i>
-                    <div class="flex-col absolute bg-webheffect px-5 py-2 rounded-lg text-custom-color-song-font text-xs hidden dropdown">
+                <p class="w-4/12 my-auto large:pl-8 mid:hidden">${album}</p>
+                <p class="w-1/12 my-auto small:hidden"><span class="material-symbols-rounded">favorite</span></p>
+                <p class="w-1/12 my-auto small:hidden">${duration}</p>
+                <div class="w-1/12 relative my-auto optcontainer hidden small:flex xsmall:text-xs">
+                    <i class="fa-solid fa-ellipsis fa-xl option text-custom-color-song-more"></i>
+                    <div class="flex-col absolute -left-12 -bottom-44 xsmall:-left-24 w-32 bg-webheffect px-5 py-2 rounded-lg text-custom-color-song-font text-xs hidden dropdown">
                         <a href="" class="py-2 mb-1 border-b-1">Add to Queue</a>
                         <a href="" class="py-1">Go to songs</a>
                         <a href="" class="py-1">Go to artist</a>
@@ -135,8 +135,8 @@ let generateAllPlaylist = () => {
     return content.innerHTML = allplaylist.map((x) => {
         let {name, acc, img} = x;
         return `
-            <div class="fex flex-col text-sm mr-8 mb-5 hover:cursor-pointer">
-                <img src="/assets/playlist-mg-assets/${img}" alt="Playlist image" class="h-44 rounded-md" >
+            <div class="fex flex-col text-sm mr-8 mb-5 hover:cursor-pointer 2xsmall:mr-5">
+                <img src="/assets/playlist-mg-assets/${img}" alt="Playlist image" class="h-44 rounded-md large:h-32 mid2:h-44 xsmall:h-36 2xsmall:h-32" >
                 <p class="text-custom-color-song-font mt-1">${name}</p>
                 <p class="text-custom-color-song-more">By ${acc}</p>
             </div>
@@ -170,12 +170,12 @@ trackshovered.forEach(track => {
 
     track.addEventListener('mouseover', () => {
         track.setAttribute('style','background-color: rgb(9, 36, 49,0.5);border-radius:50px;');
-        option.setAttribute('style','color: #797979;');
+        optcontainer.setAttribute('style', 'display: flex;');
     })
 
     track.addEventListener('mouseout', () => {
         track.setAttribute('style','background-color: #04151D;');
-        option.setAttribute('style','color: #04151D;');  
+        optcontainer.setAttribute('style', 'display: none;');
     })
 
     option.addEventListener('click', () => {
