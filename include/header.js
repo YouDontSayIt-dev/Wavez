d73dba0aeeb433c8e7aea9f88bbb009428d065a1
ImperialDuck-dev <mjmuyco13@gmail.com>
2023-05-15 01:44:21 +0800

    Pahabol front and settings responsiveness

--- include/header.js
+++ include/header.js
@@ -1,4 +1,4 @@
-class Header extends HTMLElement {
+class header extends HTMLElement {
     constructor() {
       super();
     }
@@ -7,15 +7,25 @@ class Header extends HTMLElement {
         this.innerHTML = `
         <header class = "bg-custom-color font-alata sticky">
         <div class ="flex h-[147px] items-center justify-end">
-            <div class="w-[150px] mr-auto flex items-center">
-                <img class =" px-8 py-4 " src="/assets/wavesLogo.png">
-                <div class="text-white px-4 py-4 tracking-[0.18rem] text-[44px] leading-[53px] ">WAVES</div>
+            <div class="w-auto h-auto my-1 mr-auto flex items-center ">
+                <img class ="w-[80px] h-[auto] pl-5 md:w-[40%]" src="/assets/wavesLogo.png">
+                <div class ="text-white px-2 py-2 tracking-[0.18rem] text-[24px] leading-[53px] md:text-[36px]">WAVES</div>
             </div>
-            <div class="px-6 py-4 text-white font-[19px] leading-[26px]">Login</div>
-            <div class="px-6 py-4 text-white font-[19px] leading-[26px]">Sign Up</div>
-            <div class = "px-8 py-4 text-black font-[19px] leading-[26px]">
-                <button class="bg-custom-color-sidebar-brown rounded-full py-2 px-4 font-bold">GET STARTED</button>
+            <div>
+            <h1 class="hidden px-6 py-4 text-white font-[19px] leading-[26px] md:block">Login</h1>
             </div>
+            <div>
+            <h1 class="hidden px-6 py-4 text-white font-[19px] leading-[26px] md:block">Sign Up</h1>
+            </div>
+            <div class = "px-8 py-4 text-black font-[16px] leading-[26px]">
+                <button class="hidden bg-custom-color-sidebar-brown rounded-full py-2 px-4 font-bold md:block">KNOW MORE</button>
+                
+            </div>
+            <div class="pr-5 space-y-2 mr-2 md:hidden">
+                        <div class="w-8 h-0.5 bg-white"></div>
+                        <div class="w-8 h-0.5 bg-white"></div>
+                        <div class="w-8 h-0.5 bg-white"></div>
+                      </div>
                      
         </div>
 
@@ -24,4 +34,4 @@ class Header extends HTMLElement {
     }
 }
 
-customElements.define('header-component', Header);
+customElements.define('header-component', header);
